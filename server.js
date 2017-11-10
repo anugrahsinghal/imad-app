4b4531bd541a806e103b3c0fb62f3a855cc151b2
anugrahsinghal <anugrahsinghal1@gmail.com>
2017-11-10 23:50:11 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,23 +5,44 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Anugrah Singhal'
-    heading: 'Article One'
-    date: 'Nov 11,2017'
-    content:`
-                 <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>`
+var articles = {
+    'article-one' : {
+        title: 'Article One | Anugrah Singhal'
+        heading: 'Article One'
+        date: 'Nov 11,2017'
+        content:`
+                     <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                        This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>`
+    },
+    'article-two' : {
+        title: 'Article Two | Anugrah Singhal'
+        heading: 'Article Two'
+        date: 'Nov 12,2017'
+        content:`
+                     <p>
+                        This is the content for my second article.
+                    </p>`
+    },
+    'article-three' : {
+        title: 'Article Three | Anugrah Singhal'
+        heading: 'Article Three'
+        date: 'Nov 13,2017'
+        content:`
+                     <p>
+                    
+                        This is the content for my third article.
+                    </p>`
+    }
 };
 
 function createTemplate(data) {
@@ -58,21 +79,15 @@ function createTemplate(data) {
     </html>
     `;
     return htmlTemplate;
-};
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function(req,res){
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
