897428079e921b8c6a6268d5e18e357f4f685c1a
anugrahsinghal <anugrahsinghal1@gmail.com>
2017-11-11 13:57:17 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,13 @@ element.innerHTML = 'New Value';
 
 //move the image
 
+
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft = marginLeft+10;
+    img.style.marginLeft=marginleft+'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '400px' ;
+    var interval = setInterval(moveRight, 100);
 };
