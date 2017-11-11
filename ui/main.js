1a9eed3e6d7d507dab224e6e5d54e286f2b452de
anugrahsinghal <anugrahsinghal1@gmail.com>
2017-11-11 14:35:03 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,47 @@
-console.log('Loaded!');
+//new code
+var button = document.getElementById("counter");
+
+button.onclick = function(){
+  
+  //Create a requets object
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            //render variable to correct span
+             var span= document.getElementById('count');
+             span.innerHTML = counter.toString();
+        }
+    } 
+    //not done yet
+  };
+  
+  //Make the request
+  request.open('GET', 'http://anugrahsinghal1.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old code
+
+/*console.log('Loaded!');
 
 //chhanbge the text of the main text
 
@@ -18,3 +61,10 @@ function moveRight(){
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 };
+*/
+
+
+
+
+
+
